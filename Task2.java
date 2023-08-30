3c6ffa950497eb14753c193d8c369c90641786ae
HermanYarenko <119243163+HermanYarenko@users.noreply.github.com>
2023-08-30 14:12:32 +0300

    work

---
+++ Task2.java
@@ -0,0 +1,25 @@
+public class Task2 {
+    public static void main(String[] args) {
+        foo1();
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+
+    }
+    private static void foo1() {
+    }
+    public static void printSum(Integer a, Integer b){
+        System.out.println(a + b);
+    }
+}
